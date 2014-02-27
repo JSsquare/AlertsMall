e188660b9d2f00311f78dd55ffaf38cc7b89a7dc
JohnnyPanikulam <jonysebastin@gmail.com>
2014-02-27 08:50:11 +0530

    Change in twitter keys to AlertsMall

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,7 +9,7 @@ class TweetsController < ApplicationController
     tweet_post(tweet_params)
     respond_to do |format|
       if @tweet.save
-        format.html { redirect_to new_tweet_path, notice: 'Pune Food Alerts just tweeted' }
+        format.html { redirect_to new_tweet_path, notice: 'You just tweeted for AlertsMall' }
         format.json { render action: 'shops/index', status: :created, location: @tweet }
       else
         format.html { render action: 'new' }
@@ -20,11 +20,18 @@ class TweetsController < ApplicationController
   end
 
   def tweet_post(tweet_params)
-    consumer_key = "jJm5FqTl3GJrRueIXIXOTA"
-    consumer_key_secret = "KJneLltuLLV4foKNin8SxHLcXPTDnVyvBBf9cZ2I"
+    #FOR PUNE FOOD ALERTS
+    #consumer_key = "jJm5FqTl3GJrRueIXIXOTA"
+    #consumer_key_secret = "KJneLltuLLV4foKNin8SxHLcXPTDnVyvBBf9cZ2I"
 
-    access_token = "2209559442-Lm6L4QbrcTQvl7MbGKR5YAvoefp6vxO1fTlHvAz"
-    access_token_secret = "KZ5OMcCS7n2k2a07m5rQcsLJjpEjj6bZ5hZEpySvavMHF"
+    #access_token = "2209559442-Lm6L4QbrcTQvl7MbGKR5YAvoefp6vxO1fTlHvAz"
+    #access_token_secret = "KZ5OMcCS7n2k2a07m5rQcsLJjpEjj6bZ5hZEpySvavMHF"
+
+    consumer_key = "dSMJ2nkOj87ooiWrQKzTSw"
+    consumer_key_secret = "74Q6R7yVM3E0rglcvSbWdzfnYKfSybTlkB8TO4XJY"
+
+    access_token = "2357901931-ybQJGkicqvpIYwbeYpuBu9DJUoVqzTfEVyw83JE"
+    access_token_secret = "BsOY7trOw4roXaAkz1emYp2KTzvOVvoeubuODL1MNeLQH"
 
     client = Twitter::REST::Client.new do |config|
       config.consumer_key        = consumer_key
@@ -32,8 +39,7 @@ class TweetsController < ApplicationController
       config.access_token        = access_token
       config.access_token_secret = access_token_secret
     end
-
-    client.update("This is test tweet with @gem Please Ignore")
+    client.update(tweet_params["body"])
 
 
   end
