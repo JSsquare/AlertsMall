46b237a48eb38285c5fa7b2fbd22921d16b2d3e4
JohnnyPanikulam <jonysebastin@gmail.com>
2014-03-02 20:28:23 +0530

    Migrating to Postgres DB

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ gem "therubyracer"
 gem "less-rails"
 gem "twitter-bootstrap-rails"
 
+gem 'pg'
+
 gem 'mysql'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.3'
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140223065202) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: true do |t|
     t.text     "body"
     t.datetime "created_at"
